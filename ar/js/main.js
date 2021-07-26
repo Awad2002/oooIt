88af64cf372174e9b674d52a69514f601350a995
Awad2002 <awadamr33@gmail.com>
2021-07-26 17:41:23 +0300

    fasfafasdasd

---
+++ ar/js/main.js
@@ -0,0 +1,35 @@
+$('.owl-carousel1').owlCarousel({
+    loop:true,
+    margin:70,
+    nav:true,
+    rtl: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+$('.owl-carousel2').owlCarousel({
+    loop:true,
+    rtl: true,
+    margin:38,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
